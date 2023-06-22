9a6128a03bb9185ded60be1faedd04db3c7346fc
John Nunley <jtnunley01@gmail.com>
2023-06-22 08:49:41 -0700

    fmt

--- keysym-generator/src/main.rs
+++ keysym-generator/src/main.rs
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0 OR Zlib
 // Copyright 2022-2023 John Nunley
-// 
+//
 // Licensed under the Apache License, Version 2.0, the MIT License, and
-// the Zlib license ("the Licenses"), you may not use this file except in 
+// the Zlib license ("the Licenses"), you may not use this file except in
 // compliance with one of the the Licenses, at your option. You may obtain
 //  a copy of the Licenses at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //     http://opensource.org/licenses/MIT
 //     http://opensource.org/licenses/Zlib
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the Licenses is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
